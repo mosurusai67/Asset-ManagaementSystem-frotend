c163b4591c50fbb02319c30356fbbc748c85b683
Sgayatri0519 <saimosuru0510@gmail.com>
2026-09-07 10:52:02 +0530

    ksfd

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
+// import api from "../services/api";
 import api from "../services/api";
-
 function Signup() {
 
   const navigate = useNavigate();
