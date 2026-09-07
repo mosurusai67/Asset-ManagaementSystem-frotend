f6ae2065ef63dbc2de5173138d0aad422d48d1f4
Sgayatri0519 <saimosuru0510@gmail.com>
2026-09-07 10:54:20 +0530

    kskf

--- src/pages/Assets.jsx
+++ src/pages/Assets.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-import api from "../services/Api";
+import api from "../services/apilink";
 import AssetCard from "../components/AssertCard";
 // import AssetCard from "../components/AssetCard.jsx";
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 // import api from "../services/api";
-import api from "../services/api";
+import api from "../services/apilink";
 function Signup() {
 
   const navigate = useNavigate();
