3fef85309f4ac9c013397044374761b31b6efa31
Sgayatri0519 <saimosuru0510@gmail.com>
2026-09-07 11:00:13 +0530

    ksk

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,8 +6,8 @@ import AddAsset from "../pages/AddAsset";
 import EditAsset from "../pages/EditAsset";
 import Favorites from "../pages/Favorites";
 import Home from "../pages/Home ";
-import Login from "../pages/login";
-import Signup from "../pages/signup";
+import Login from "../pages/Loginform";
+import Signup from "../pages/Signupform";
 
 
 
